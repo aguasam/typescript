9d35ab3189158589c25c53b0db11473f05520dfb
aguasam <carlos.cvfp@gmail.com>
2022-03-09 09:22:05 -0300

    master

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,45 @@
-export function decodedValue(color1: string) {
+export function decodedValue(colorVector:string) {
+  switch(colorVector){
+    case 'black':
+      var resp = '0'
+      break;
+
+    case 'brown':
+      var resp = '1'
+      break;
+
+    case 'red':
+      var resp = '2'
+      break;
+
+    case 'orange':
+      var resp = '3'
+      break;
+
+    case 'yellow':
+      var resp = '4'
+      break;
+
+    case 'green':
+      var resp = '5'
+      break;
+
+    case 'blue':
+      var resp = '6'
+      break;
+
+    case 'violet':
+      var resp = '07'
+      break;
+
+    case 'grey':
+      var resp = '8'
+      break;
+
+    case 'white':
+      var resp = '9'
+      break;
+  }
+
   throw new Error('Remove this statement and implement this function')
 }
