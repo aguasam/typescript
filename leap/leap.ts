4778f9e5504ea3ad4dca18cec3089f80b384b4dc
aguasam <carlos.cvfp@gmail.com>
2022-03-11 10:16:03 -0300

    master

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,4 @@
 export function isLeap(ano: number) {
-  //throw new Error('Remove this statement and implement this function')
   if((ano%4 == 0 && ano%100!=0) || ano%400 == 0){
     return true;
   }else{
