c6f34706b4674d15dbc50ef9056375cbe3b62178
aguasam <carlos.cvfp@gmail.com>
2022-03-24 14:15:23 -0300

    master

--- hello-world/hexa.js
+++ hello-world/hexa.js
@@ -1,7 +1,12 @@
-for(var i=1;i<200;i++){
-    var Hexa = i
-    var SHexa = 
+for(var i=1;i<5;i++){
+    var SHexa = i.toString()
+    for(let z = SHexa.length; z<4; z++){
+        SHexa = '0' + SHexa
+    }
+    SHexa = SHexa.split('')
+    var Hexa = SHexa[0] + SHexa[1] + ':' + SHexa[2] + SHexa[3]
 
 }
 
+console.log(SHexa)
 console.log(Hexa)
